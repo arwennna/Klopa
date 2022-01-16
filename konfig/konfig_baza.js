578c9e71159fcb377503b746bf49e1f154c671e0
Sara-Farah Materne <75076992+arwennna@users.noreply.github.com>
2022-01-16 14:34:25 +0100

    Update konfig_baza.js

--- konfig/konfig_baza.js
+++ konfig/konfig_baza.js
@@ -1,10 +1,10 @@
 var pg = require('pg');
 
 var config = {
-    user: 'xyehoygy',
-    database: 'xyehoygy',
-    password: 'GD6Pj-0UammOKUTrHO2ZnC8BZFSy3YoG',
-    host:   'rogue.db.elephantsql.com',
+    user: 'xxxxxxxx',
+    database: 'xxxxxxxx',
+    password: 'xxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxx',
+    host:   'xxxxx.db.elephantsql.com',
     port: 5432,
     max: 1000,
     idleTimeoutMillis: 30000
